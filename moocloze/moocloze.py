a0375748dcd136e11cab7abda37d4bebc7007ea1
Miguel Hernández-Cabronero <miguel.hernandez@uab.cat>
2023-10-17 22:33:43 +0200

    Added support for multiresponse (checkbox) answers

--- moocloze/moocloze.py
+++ moocloze/moocloze.py
@@ -71,16 +71,49 @@ class Numerical(Field):
     """
     # Correct numerical value. Consider a non-zero tolerance for non-integer answers.
     answer: int
-    # Weight given to the response in this field. Useful when more than one field is employed.
-    weight: int = 1
     # Absolute error tolerance. For instance, if the answer is 10 and tolerance is 0.05, any
     # value between 9.95 and 10.05 will be accepted as valid by the system
     tolerance: float = 0
     # If True, any non-zero tolerance will be displayed after the input field
     show_tolerance: bool = True
+    # Weight given to the response in this field. Useful when more than one field is employed.
+    weight: int = 1
 
     def __str__(self):
         s = f"{{{self.weight}:NUMERICAL:={self.answer}:{self.tolerance}}}"
         if self.show_tolerance and self.tolerance > 0:
             s += f" (\\(\\pm {self.tolerance}\\))"
+        return s
+
+
+@_dataclass
+class Multiresponse(Field):
+    """Multiple options with checkboxes, where none, some or all may be correct.
+    """
+    # List of answer strings that must be checked (correct)
+    correct_answers: _List[str]
+    # List of answer strings that must not be checked (incorrect)
+    incorrect_answers: _List[str]
+    # If True, answers are shown horizontally. Otherwise, vertically
+    horizontal: bool = True
+    # If True, answers are shuffled. Otherwise, they are displayed in alphabetical order
+    shuffle: bool = True
+    # Weight given to the response in this field. Useful when more than one field is employed.
+    weight: int = 1
+
+    def __str__(self):
+        assert len(self.correct_answers) + len(self.incorrect_answers) > 0, \
+            "At least one correct or incorrect answer must be specified"
+        s = (f"{{{self.weight}:MULTIRESPONSE"
+             f"{'_' if self.horizontal or self.shuffle else ''}"
+             f"{'H' if self.horizontal else ''}"
+             f"{'S' if self.shuffle else ''}"
+             f":")
+        text_correct_tuples = []
+        text_correct_tuples += [(text, True) for text in self.correct_answers]
+        text_correct_tuples += [(text, False) for text in self.incorrect_answers]
+        if not self.shuffle:
+            text_correct_tuples = sorted(text_correct_tuples)
+        s += "~".join(f"{'=' if correct else ''}{text}" for text, correct in text_correct_tuples)
+        s += "}"
         return s
